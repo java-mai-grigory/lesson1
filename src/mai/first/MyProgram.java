17d90a5875224cef7198dfc3a584484d83cace94
Григорий <@DESKTOP-5A5QD7V>
2018-02-27 02:44:57 +0300

    Изменил приветствие

--- src/mai/first/MyProgram.java
+++ src/mai/first/MyProgram.java
@@ -4,5 +4,6 @@ public class MyProgram {
     static public void main(String[] arg)
     {
         System.out.println("Моя первая программа!!!");
+        System.out.println("Программа выполнена!!!");
     }
 }
