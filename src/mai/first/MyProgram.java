d4fafef865222d2c5643f8966727dc937835cc48
java-mai-grigory <36867358+java-mai-grigory@users.noreply.github.com>
2018-02-27 03:00:28 +0300

    Update MyProgram.java

--- src/mai/first/MyProgram.java
+++ src/mai/first/MyProgram.java
@@ -4,6 +4,5 @@ public class MyProgram {
     static public void main(String[] arg)
     {
         System.out.println("Моя первая программа!!!");
-        System.out.println("Программа выполнена!!!");
     }
 }
